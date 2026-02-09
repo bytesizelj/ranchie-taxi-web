b762368fc605cff10b5ffd7b6402c1cbd9050ded
Leslieanna Joseph <bytesofknowledgelj@gmail.com>
2026-02-08 21:33:44 -0400

    Fix Firebase version mismatch in service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/12.9.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/12.9.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyC49BbFJhjpHajencr1W6VlpiFwpWTeD8U",
